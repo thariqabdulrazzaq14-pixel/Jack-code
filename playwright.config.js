cc3c4c901cc31e080ede1a184ed88271125bdf8e
thariqabdulrazzaq14-pixel <thariqabdulrazzaq14@gmail.com>
2026-04-21 01:14:55 +0700

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,19 @@
+const { defineConfig, devices } = require('@playwright/test');
+const path = require('path');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: 0,
+  reporter: 'list',
+  use: {
+    baseURL: 'file://' + path.resolve(__dirname, 'index.html'),
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
